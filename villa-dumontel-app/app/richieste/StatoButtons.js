6ef95818e054448bed58a9394bdea34ac7026cce
Villadumontel2026 <villadumontel@gmail.com>
2026-07-31 16:02:39 +0200

    Refactor StatoButtons to use fragment instead of span
    Refactor StatoButtons component to remove unnecessary span.

--- villa-dumontel-app/app/richieste/StatoButtons.js
+++ villa-dumontel-app/app/richieste/StatoButtons.js
@@ -6,16 +6,12 @@ const STATI = ["nuova", "in gestione", "confermata", "completata"];
 
 export default function StatoButtons({ id, statoAttuale }) {
   return (
-    <span style={{ marginLeft: "0.75rem" }}>
+    <>
       {STATI.filter((s) => s !== statoAttuale).map((s) => (
-        <button
-          key={s}
-          onClick={() => aggiornaStato(id, s)}
-          style={{ marginRight: "0.25rem", fontSize: "12px" }}
-        >
+        <button key={s} onClick={() => aggiornaStato(id, s)}>
           &rarr; {s}
         </button>
       ))}
-    </span>
+    </>
   );
 }
